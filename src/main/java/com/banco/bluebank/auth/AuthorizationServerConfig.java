aa186b0c1676f942982e7248e28d15cd113e1be4
Carlos Betiol <carlos@sistelpa.com>
2021-12-07 16:08:54 -0300

    inicio do projeto de geracao de token, a principio opaco

---
+++ src/main/java/com/banco/bluebank/auth/AuthorizationServerConfig.java
@@ -0,0 +1,50 @@
+package com.banco.bluebank.auth;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
+
+@Configuration
+@EnableAuthorizationServer
+public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdapter {
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @Override
+    public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
+
+        clients.inMemory()
+                .withClient("bluebank-app-web")
+                    .secret(passwordEncoder.encode("web123"))
+                    .authorizedGrantTypes("password")
+                    .scopes("ADMIN")
+                    .accessTokenValiditySeconds(60*60*12)
+                .and()
+                .withClient("bluebank-app-mobile")
+                    .secret(passwordEncoder.encode("mob123"))
+                    .authorizedGrantTypes("password")
+                    .scopes("CUSTOMER")
+                    .accessTokenValiditySeconds(60*60*12);
+    }
+
+    @Override
+    public void configure(AuthorizationServerSecurityConfigurer security) throws Exception {
+//        security.checkTokenAccess("isAuthenticated()");
+        security.checkTokenAccess("permitAll()");
+    }
+
+    @Override
+    public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
+        endpoints.authenticationManager(authenticationManager);
+    }
+}
---
+++ src/main/java/com/banco/bluebank/auth/WebSecurityConfig.java
@@ -0,0 +1,39 @@
+package com.banco.bluebank.auth;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.inMemoryAuthentication()
+			.withUser("carlos")
+				.password(passwordEncoder().encode("123"))
+				.roles("ADMIN")
+			.and()
+			.withUser("pedro")
+				.password(passwordEncoder().encode("123"))
+				.roles("USER");
+	}
+	
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+
+	@Bean
+	@Override
+	protected AuthenticationManager authenticationManager() throws Exception {
+		return super.authenticationManager();
+	}
+
+}
