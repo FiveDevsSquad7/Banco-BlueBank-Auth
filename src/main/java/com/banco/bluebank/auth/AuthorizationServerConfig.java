7ceea7daea0b67ec65637b8033b88ff2b40c9481
Carlos Betiol <carlos@sistelpa.com>
2021-12-08 00:43:12 -0300

    implementacao parcial de authorization password oauth2 com jwt

--- src/main/java/com/banco/bluebank/auth/AuthorizationServerConfig.java
+++ src/main/java/com/banco/bluebank/auth/AuthorizationServerConfig.java
@@ -1,6 +1,7 @@
 package com.banco.bluebank.auth;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -9,6 +10,7 @@ import org.springframework.security.oauth2.config.annotation.web.configuration.A
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
+import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
 
 @Configuration
 @EnableAuthorizationServer
@@ -34,7 +36,10 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
                     .secret(passwordEncoder.encode("mob123"))
                     .authorizedGrantTypes("password")
                     .scopes("CUSTOMER")
-                    .accessTokenValiditySeconds(60*60*12);
+                    .accessTokenValiditySeconds(60*60*12)
+                .and()
+                .withClient("checktoken")
+                    .secret(passwordEncoder.encode("check978"));
     }
 
     @Override
@@ -43,8 +48,18 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
         security.checkTokenAccess("permitAll()");
     }
 
+    @Bean
+    public JwtAccessTokenConverter jwtAccessTokenConverter() {
+        JwtAccessTokenConverter jwtAccessTokenConverter = new JwtAccessTokenConverter();
+        jwtAccessTokenConverter.setSigningKey("fjalsdjflsdafj34534lkjdfoaj23450923450j03ffjvvjfj");
+
+        return jwtAccessTokenConverter;
+    }
+
     @Override
     public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
-        endpoints.authenticationManager(authenticationManager);
+        endpoints
+                .authenticationManager(authenticationManager)
+                .accessTokenConverter(jwtAccessTokenConverter());
     }
 }
