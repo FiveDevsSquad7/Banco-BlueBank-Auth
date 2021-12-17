2c6b9ff71638f3c09eca2295a8002808c8113974
Carlos Betiol <carlos@sistelpa.com>
2021-12-17 13:21:10 -0300

    retirada do arquivo jks do projeto o transformando em recurso direto dentro do código

--- src/main/java/com/banco/bluebank/auth/BlueBankAuthApplication.java
+++ src/main/java/com/banco/bluebank/auth/BlueBankAuthApplication.java
@@ -1,13 +1,22 @@
 package com.banco.bluebank.auth;
 
+import com.banco.bluebank.auth.core.io.Base64ProtocolResolver;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.TimeZone;
 
 @SpringBootApplication
 public class BlueBankAuthApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(BlueBankAuthApplication.class, args);
+        TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
+
+        var app = new SpringApplication(BlueBankAuthApplication.class);
+        app.addListeners(new Base64ProtocolResolver());
+        app.run(args);
+
+//        SpringApplication.run(BlueBankAuthApplication.class, args);
     }
 
 }
